ad447cc9518f059659bafa19ff773e74e0ba9e1c
Judit Acs <judit@mokk.bme.hu>
2015-11-17 10:13:24 +0100

    python2 version

---
+++ wordcount.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from sys import stdin
+
+
+def word_count():
+    counter = defaultdict(int)
+    for l in stdin:
+        for word in l.split():
+            counter[word] += 1
+    return
+    for word, cnt in sorted(counter.iteritems(), key=lambda x: (-x[1], x[0])):
+        print('{0}\t{1}'.format(word, cnt))
+
+if __name__ == '__main__':
+    word_count()
