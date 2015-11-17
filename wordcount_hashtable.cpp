09fb5ff213708bf502516bf530bdc0e428a7fc9f
Judit Acs <judit@mokk.bme.hu>
2015-11-17 10:13:13 +0100

    cpp with unordered map

---
+++ wordcount_hashtable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <set>
+
+
+class F {
+public:
+    bool operator()(std::pair<int, std::string> lhs, std::pair<int, std::string> rhs) {
+        if (lhs.first > rhs.first) return true;
+        if (lhs.first == rhs.first && lhs.second < rhs.second) return true;
+        return false;
+    }
+};
+
+int main() {
+    std::unordered_map<std::string, int> m;
+    std::string s;
+    while (std::cin >> s)
+        ++m[s];
+    std::multiset<std::pair<int, std::string>, F> mm;
+    for (auto p: m) mm.insert(std::pair<int, std::string>{p.second, p.first});
+    for (auto p: mm) 
+        std::cout << p.second << "\t" << p.first << std::endl;
+
+}
