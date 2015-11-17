74b6f79b8df38864f5008a8a1fa97b0230165263
Braun Patrik <bra.patrik@gmail.com>
2015-11-17 11:44:24 +0100

    wordcount updated

--- wordcount.php
+++ wordcount.php
@@ -1,15 +1,5 @@
 <?php
-
-function countWord($word,$array){
-
-    if (!array_key_exists($word,$array)){
-        $array[$word] = 1;
-        return;
-    }
-    $array[$word]++;
-}
-
-
+ini_set('memory_limit', '2048M');
 $stdin = fopen('php://stdin', 'r');
 //$stdin = fopen('de', 'r');
 
@@ -17,7 +7,7 @@ $stdin = fopen('php://stdin', 'r');
 
 $array = array();
 while (false !== ($line = fgets($stdin))) {
-    $words = explode(" ", $line);
+    $words = explode(" ", trim($line));
 
     foreach($words as $word){
         if (!array_key_exists($word,$array)){
@@ -28,8 +18,25 @@ while (false !== ($line = fgets($stdin))) {
     }
 
 }
-ksort($array);
-arsort($array);
+
+$array2 = array();
+
 foreach($array as $key => $value){
-    echo $key."\t".$value."\n";
+
+    if (!array_key_exists($value,$array2)){
+        $array2[$value] = array($key);
+        continue;
+    }
+    $array2[$value][] = $key;
+}
+
+//ksort($array);
+//arsort($array);
+
+krsort($array2);
+foreach($array2 as $count => $wordsArray){
+    sort ($wordsArray);
+    foreach($wordsArray as $word) {
+        echo $word . "\t" . $count . "\n";
+    }
 }
