be5cb8d7da0723e9d31067d871b438a4b3032519
Judit Acs <judit@mokk.bme.hu>
2015-11-18 11:40:35 +0100

    cpp11 using std::vector and std::sort

---
+++ cpp/wordcount_vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+
+class F {
+public:
+    bool operator()(std::pair<int, std::string> lhs, std::pair<int, std::string> rhs) {
+        if (lhs.first > rhs.first) return true;
+        if (lhs.first == rhs.first && lhs.second < rhs.second) return true;
+        return false;
+    }
+};
+
+int main() {
+    std::unordered_map<std::string, int> m;
+    std::string s;
+    std::ios_base::sync_with_stdio(false);
+    while (std::cin >> s) {
+        ++m[s];
+    }
+    std::vector<std::pair<int, std::string>> mvec;
+    for (auto p: m) mvec.push_back(std::pair<int, std::string>{p.second, p.first});
+    std::sort(mvec.begin(), mvec.end(), F());
+    for (auto p: mvec) {
+        std::cout << p.second << "\t" << p.first << std::endl;
+        printf("%d\t%s\n", p.first, p.second.c_str());
+    }
+
+}
