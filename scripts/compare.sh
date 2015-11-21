09e4ee11dfe9960748a3bdd94475584e2ad7b013
Judit Acs <judit@mokk.bme.hu>
2015-11-21 20:47:08 +0100

    trim output

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -9,7 +9,7 @@ fi
 cat run_commands.txt | while read line; do
     echo $line
     for i in {1..1}; do
-        zcat data/romanian_100k.gz | eval /usr/bin/time -f "%e__%U__%S__%M" $line > out 2>stat
+        zcat data/romanian_100k.gz | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
         echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
     done
 done
