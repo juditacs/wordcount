4af8a482dd603adcfc09eb0e433d235f8d2d4ea1
Judit Acs <judit@mokk.bme.hu>
2015-11-21 22:34:12 +0100

    run each test n times

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -8,7 +8,7 @@ fi
 
 cat run_commands.txt | while read line; do
     echo $line
-    for i in {1..1}; do
+    for i in {1..$n}; do
         zcat data/romanian_100k.gz | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
         echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
     done
