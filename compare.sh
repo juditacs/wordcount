0da00e307f62feba85498c9a7ed6273347a18ecc
Judit Acs <judit@mokk.bme.hu>
2015-11-21 14:52:19 +0100

    change input file name

---
+++ compare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ]; then
+	n=3
+else
+	n=$1
+fi
+
+cat run_commands.txt | while read line; do
+    echo $line
+    for i in {1..1}; do
+        zcat data/romanian_100k.gz | eval /usr/bin/time -f "%e__%U__%S__%M" $line > out 2>stat
+        echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
+    done
+done
--- create_input.sh
+++ create_input.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-wget http://hlt.sztaki.hu/resources/corp/romanian.tok.gz data/
-zcat data/romanian.tok.gz | grep -v ^DOC | head -1000 > data/romanian_1000.txt
-zcat data/romanian.tok.gz | grep -v ^DOC | head -100000 | gzip > data/romanian_100k.gz
-zcat data/romanian.tok.gz | grep -v ^DOC | head -1000000 | gzip > data/romanian_1M.gz
+wget http://avalon.aut.bme.hu/~judit/files/romanian_1M.gz data/
+zcat data/romanian_1M.gz | head -100000 | gzip > data/romanian_100k.gz
