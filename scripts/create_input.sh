4794d438a1c624cd3d35044432800206dd96a829
Judit Acs <judit@mokk.bme.hu>
2015-11-21 15:14:35 +0100

    move wget output to data

--- scripts/create_input.sh
+++ scripts/create_input.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-wget http://avalon.aut.bme.hu/~judit/files/romanian_1M.gz data/
+wget http://avalon.aut.bme.hu/~judit/files/romanian_1M.gz
+mv romanian_1M.gz data/
 zcat data/romanian_1M.gz | head -100000 | gzip > data/romanian_100k.gz
