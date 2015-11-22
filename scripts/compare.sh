5d45caf337ef0860e4a9f452d944de29be3ede0f
Judit Acs <judit@mokk.bme.hu>
2015-11-22 11:07:10 +0100

    add parameters and usage info

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
 if [ -z $1 ]; then
+    echo "Usage: compare.sh [input file] [testnum=3]"
+    exit
+fi
+
+data=$1
+
+if [ -z $2 ]; then
 	n=3
 else
-	n=$1
+	n=$2
 fi
 
 cat run_commands.txt | while read line; do
     echo $line
     for i in {1..$n}; do
-        zcat data/romanian_100k.gz | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
+        zcat $data | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
         echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
     done
 done
