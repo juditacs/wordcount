7253d166e010a89eadfb2fd4c6e0157188fc0f6f
Judit Acs <judit@mokk.bme.hu>
2015-11-22 13:42:11 +0100

    do not break on non-breakable space

--- python/wordcount_py3.py
+++ python/wordcount_py3.py
@@ -9,10 +9,10 @@ stdin = codecs.getreader('utf8')(stdin.detach(), errors='ignore')
 def word_count():
     counter = defaultdict(int)
     for l in stdin:
-        for word in l.split():
+        for word in bytes(l, 'utf8').split():
             counter[word] += 1
     for word, cnt in sorted(counter.items(), key=lambda x: (-x[1], x[0])):
-        print('{0}\t{1}'.format(word, cnt))
+        print('{0}\t{1}'.format(word.decode('utf8'), cnt))
 
 if __name__ == '__main__':
     word_count()
