f738dcf02a7384a86c0530a9f045c337a810341a
Judit Acs <judit@mokk.bme.hu>
2015-11-23 14:34:59 +0100

    test file can be plain text or gz

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -13,10 +13,16 @@ else
 	n=$2
 fi
 
+if [[ "$data" == *gz ]]; then
+    command=zcat
+else
+    command=cat
+fi
+
 cat run_commands.txt | while read line; do
     echo $line
     for i in $( seq 1 $n); do
-        zcat $data | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
+        $command $data | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
         echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
     done
 done
