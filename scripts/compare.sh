f8634b056c5a5f3680968f9871f8ce937c1ebe1f
Judit Acs <judit@mokk.bme.hu>
2015-11-23 11:33:11 +0100

    for range bug fixed

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -15,7 +15,7 @@ fi
 
 cat run_commands.txt | while read line; do
     echo $line
-    for i in {1..$n}; do
+    for i in $( seq 1 $n); do
         zcat $data | eval /usr/bin/time -f "%e__%U__%M" $line > out 2>stat
         echo ${line}__$(cat stat) | sed 's/__/\t/g' >> results.txt
     done
