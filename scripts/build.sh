c799a87d12ed7b349582f5e15b04e69c57683a36
Judit Acs <judit@mokk.bme.hu>
2015-11-26 14:53:54 +0100

    compile cpp with O3

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cd cpp
-g++ wordcount_map.cpp -std=c++11 -o wc_baseline
-g++ wordcount_hashtable.cpp -std=c++11 -o wc_baseline_hash
-g++ wordcount_hashtable_nosync_stdio.cpp -std=c++11 -o wc_hash_nosync
-g++ wordcount_vector.cpp -std=c++11 -o wc_vector
+g++ wordcount_map.cpp -std=c++11 -o wc_baseline -O3
+g++ wordcount_hashtable.cpp -std=c++11 -o wc_baseline_hash -O3
+g++ wordcount_hashtable_nosync_stdio.cpp -std=c++11 -o wc_hash_nosync -O3
+g++ wordcount_vector.cpp -std=c++11 -o wc_vector -O3
 
 cd ../java
 javac WordCount.java
