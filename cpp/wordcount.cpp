45602eb3e421a52ce3c99c7b2b44aeb4813e136c
gaebor <gaebor@gmail.com>
2016-03-20 23:21:51 +0100

    reduced memory usage and scanf, printf

--- cpp/wordcount.cpp
+++ cpp/wordcount.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
 #include <string>
 
 typedef std::unordered_map<std::string, size_t> Hash;
-typedef std::pair<std::string, size_t> Entry;
+typedef Hash::value_type Entry;
+
+#define MAX_WORD_LENGTH 1024
+char word[MAX_WORD_LENGTH];
 
 int main() {
 	Hash m;
-    std::string s;
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-    while (std::cin >> s) {
-        ++m[s];
-    }
-	std::vector<Entry> mvec(m.begin(), m.end());
-    std::sort(mvec.begin(), mvec.end(),
-		[](const Entry& left, const Entry& right)
-	{
-		return (left.second > right.second||
-			(!(right.second > left.second ) && left.first < right.first));
-	});
-    for (auto p: mvec) {
-        std::cout << p.first << "\t" << p.second << "\n";
-    }
 
+	char format[MAX_WORD_LENGTH];
+	sprintf(format, "%%%ds", MAX_WORD_LENGTH - 1);
+
+	while (scanf(format, word) == 1)
+		++m[word];
+
+	std::vector<const Entry*> mvec(m.size());
+	std::transform(m.begin(), m.end(), mvec.begin(), [](const Entry& original){return &original;});
+	std::sort(mvec.begin(), mvec.end(),
+		[](const Entry*& left, const Entry*& right)
+		{
+			return (left->second > right->second||
+				(!(right->second > left->second ) && left->first < right->first));
+		});
+	for (auto p: mvec)
+		printf("%s\t%u\n", p->first.c_str(), p->second);
+
+	return 0;
 }
