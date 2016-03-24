ae3c9ecb9393c5f26e82e39771cfc929cb196f75
TPoise <tpoise@gmail.com>
2016-03-24 00:10:36 -0500

    1.  Presizing the dictionary to prevent re-sizes 2.  Using a unsigned 32-bit integer for counting 3.  Reducing unnecessary checks on the dictionary to determine if an item is already there. 4.  Using parallel sorting

---
+++ csharp/WordCount_TPoise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCount_TPoise
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var wordCounts = new Dictionary<string, uint>(Convert.ToInt32(Math.Pow(2.0, 16.0)));
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                uint count = 0;
+
+                foreach (string word in line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (wordCounts.TryGetValue(word, out count))
+                        wordCounts[word] = ++count;
+                    else
+                        wordCounts[word] = 1;
+
+                }
+            }
+
+            foreach (var wordCount in wordCounts.AsParallel().OrderByDescending(k => k.Value).ThenBy(k => k.Key).Take(10))
+            {
+                Console.WriteLine("{0}\t{1}", wordCount.Key, wordCount.Value);
+            }
+
+        }
+        
+    }
+}
