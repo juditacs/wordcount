ca018ec888ac7e4bbb2f1599d5c8a6dd99107f0b
belaustegui <cristian.alvb@gmail.com>
2016-03-25 12:04:59 +0100

    Add script for wordcount in Elixir

---
+++ elixir/wordcount.exs
@@ -0,0 +1,24 @@
+IO.stream(:stdio, :line)
+# Create a dictionary in which the keys are the words and the values are
+# the number of appearances in the input text.
+|> Enum.reduce(%{}, fn (line, acc) ->
+  line
+  |> String.split
+  |> Enum.reduce(acc, fn (word, counter) ->
+    case Dict.get(counter, word) do
+      nil -> Dict.put(counter, word, 1)
+      prev_count -> Dict.put(counter, word, prev_count + 1)
+    end
+  end)
+end)
+# Transform the dictionary into a list of tuples with the form {word, appearances}
+|> Dict.to_list
+# Sort words (if appearance count are equal, sort alphabetically)
+|> Enum.sort(fn ({word1, count1}, {word2, count2}) ->
+  cond do
+    count2 == count1 -> word1 < word2
+    true -> count1 > count2
+  end
+end)
+# Print the results
+|> Enum.each(fn ({word, count}) -> IO.puts("#{word}\t#{count}") end)
