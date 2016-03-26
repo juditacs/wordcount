7ea1180a5534a6210f6b81766cbbe02fbd516fe1
gaebor <gaebor@gmail.com>
2016-03-26 23:54:51 +0100

    went back to cin and cout
    @juditacs pointed out that scanf and printf are not c++

--- cpp/wordcount.cpp
+++ cpp/wordcount.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
@@ -7,28 +7,26 @@
 typedef std::unordered_map<std::string, size_t> Hash;
 typedef Hash::value_type Entry;
 
-#define MAX_WORD_LENGTH 1024
-char word[MAX_WORD_LENGTH];
-
-int main() {
+int main()
+{
 	Hash m;
-
-	char format[MAX_WORD_LENGTH];
-	sprintf(format, "%%%ds", MAX_WORD_LENGTH - 1);
-
-	while (scanf(format, word) == 1)
-		++m[word];
+	std::string s;
+	
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	while (std::cin >> s)
+		++m[s];
 
 	std::vector<const Entry*> mvec(m.size());
 	std::transform(m.begin(), m.end(), mvec.begin(), [](const Entry& original){return &original;});
 	std::sort(mvec.begin(), mvec.end(),
-		[](const Entry*& left, const Entry*& right)
+		[](const Entry* left, const Entry* right)
 		{
 			return (left->second > right->second||
 				(!(right->second > left->second ) && left->first < right->first));
 		});
 	for (auto p: mvec)
-		printf("%s\t%u\n", p->first.c_str(), p->second);
+		std::cout << p->first << "\t" << p->second << "\n";
 
 	return 0;
 }
