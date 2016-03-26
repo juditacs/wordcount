cfb94e12d4b59a833cfe5a0052b2ddd162cea9f1
gaebor <gaebor@gmail.com>
2016-03-26 20:20:18 +0100

    c implementation

---
+++ c/wordcount.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef MAX_WORD_LENGTH
+#	define MAX_WORD_LENGTH 1000
+#endif
+
+size_t hash_function(const char* str, size_t len)
+{
+	size_t h = 0,i;
+	for (i = 0; i < len; i += sizeof(size_t))
+	{
+		h += *((size_t*)(str + i)); //modulo is taken via unsigned integer overflow!
+	}
+	return h;
+}
+
+typedef struct REC
+{
+	char* str;
+	size_t count;
+} hash_rec;
+
+size_t hash_table_size = 4096, actual_hash_size = 0;
+hash_rec* hash_table;
+const float rehash_constant = 0.8f;
+const float expand_constant = 1.5f;
+
+void rehash()
+{
+	hash_rec* what, *new_place;
+	const size_t new_table_size = (size_t)(expand_constant*hash_table_size);
+	size_t new_hash_val;
+	hash_rec* new_table = calloc(new_table_size, sizeof(hash_rec));
+
+	if (new_table == NULL)
+		exit(1);
+	for (what = hash_table; what < hash_table + hash_table_size; ++what)
+		if (what->str)
+		{
+			new_hash_val = hash_function(what->str, strlen(what->str)) % new_table_size;
+			for (new_place = new_table + new_hash_val; new_place < new_table + new_table_size; ++new_place)
+			{
+				if (new_place->str == NULL)
+				{
+					*new_place = *what;
+					break;
+				}
+			}
+			if (new_place == new_table + new_table_size) // start from the beginning
+				for (new_place = new_table; new_place < new_table + new_hash_val; ++new_place)
+				{
+					if (new_place->str == NULL)
+					{
+						*new_place = *what;
+						break;
+					}
+				}
+		}
+	hash_table_size = new_table_size;
+	free(hash_table);
+	hash_table = new_table;
+}
+
+typedef enum {
+	INSERTED,
+	INCREMENTED,
+	COLLISION
+} INSERT_RESULT;
+
+INSERT_RESULT compare_hash(hash_rec* where, const char* str, size_t len)
+{
+	if (where->str == NULL)
+	{
+		where->str = calloc(len + sizeof(size_t), sizeof(char));
+		memcpy(where->str, str, len + sizeof(size_t));
+		where->count = 1;
+		++actual_hash_size;
+		return INSERTED;
+	}
+	else if (strcmp(where->str, str) == 0)
+	{
+		++(where->count);
+		return INCREMENTED;
+	}else
+		return COLLISION;
+}
+
+void hash_insert(const char* str, size_t len, hash_rec* hash_table, size_t hash_table_size)
+{
+	size_t hash_val = hash_function(str, len) % hash_table_size;
+	hash_rec* where;
+	for (where = hash_table + hash_val; where < hash_table + hash_table_size; ++where)
+	{
+		switch (compare_hash(where, str, len))
+		{
+		case INCREMENTED:
+		case INSERTED:
+			return;
+		case COLLISION: break;
+		}
+	}
+	for (where = hash_table; where < hash_table + hash_val; ++where)
+	{
+		// try the same from the beginning
+		switch (compare_hash(where, str, len))
+		{
+		case INCREMENTED:
+		case INSERTED:
+			return;
+		case COLLISION: break;
+		}
+	}
+}
+
+int comparator(const void* left, const void* right)
+{
+	return ((*(hash_rec**)left)->count > (*(hash_rec**)right)->count) ? -1 : (((*(hash_rec**)left)->count == (*(hash_rec**)right)->count) ? strcmp((*(hash_rec**)left)->str, (*(hash_rec**)right)->str) : 1);
+}
+
+int main(int argc, char* argv[])
+{
+	char word[MAX_WORD_LENGTH + sizeof(size_t)];
+	char input_format[MAX_WORD_LENGTH];
+	const char* output_format = (sizeof(size_t) == sizeof(long long) ? "%s\t%llu\n" : "%s\t%u\n");
+
+	size_t len;
+	hash_rec* hash_ptr, **sorted_table;
+	char* const program_name = argv[0];
+
+	for (++argv; *argv; ++argv)
+	{
+		if (strcmp("-h", *argv) == 0 || strcmp("--help", *argv) == 0)
+		{
+			printf("Simple word counting application, author: borbely@math.bme.hu\nUSAGE: %s [options] < your.favorite.text.txt > words.and.counts.txt\n", program_name);
+			exit(0);
+		}
+	}
+
+	hash_table = calloc(hash_table_size, sizeof(hash_rec));
+	
+	sprintf(input_format, "%%%ds", MAX_WORD_LENGTH);
+
+	while (scanf(input_format, word) == 1)
+	{
+		len = strlen(word);
+		*((size_t*)(word + len)) = 0;
+		hash_insert(word, len, hash_table, hash_table_size);
+		if (actual_hash_size > rehash_constant*hash_table_size)
+		{
+			rehash();
+		}
+	}
+
+	sorted_table = malloc(actual_hash_size*sizeof(hash_rec*));
+	if (sorted_table == NULL)
+		exit(1);
+	len = 0;
+	for (hash_ptr = hash_table; hash_ptr < hash_table + hash_table_size; ++hash_ptr)
+	{
+		if (hash_ptr->str)
+			sorted_table[len++] = hash_ptr;
+	}
+	qsort(sorted_table, actual_hash_size, sizeof(hash_rec*), comparator);
+
+	for (len = 0; len < actual_hash_size; ++len)
+		printf(output_format, sorted_table[len]->str, sorted_table[len]->count);
+
+	return 0;
+}
