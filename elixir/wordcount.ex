66e92bb67d05970a366d19845bf612985d296cdb
belaustegui <cristian.alvb@gmail.com>
2016-03-26 11:34:44 +0100

    Substitute Dict usages with Map
    The Dict module is now deprecated in favour of the Map module.

--- elixir/wordcount.ex
+++ elixir/wordcount.ex
@@ -8,15 +8,15 @@ IO.stream(:stdio, :line)
     if String.length(word) == 0 do
       counter
     else
-      case Dict.get(counter, word) do
-        nil -> Dict.put(counter, word, 1)
-        prev_count -> Dict.put(counter, word, prev_count + 1)
+      case Map.get(counter, word) do
+        nil -> Map.put(counter, word, 1)
+        prev_count -> Map.put(counter, word, prev_count + 1)
       end
     end
   end)
 end)
 # Transform the dictionary into a list of tuples with the form {word, appearances}
-|> Dict.to_list
+|> Map.to_list
 # Sort words (if appearance count are equal, sort alphabetically)
 |> Enum.sort(fn ({word1, count1}, {word2, count2}) ->
   cond do
