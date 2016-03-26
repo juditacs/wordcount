bf91e529c6a5fd4082674e04bbe46590a22db57f
Judit Acs <judit@mokk.bme.hu>
2016-03-26 21:17:18 +0100

    print contributors in eval table

--- scripts/evaluate_results.py
+++ scripts/evaluate_results.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python2
 from sys import argv, stdin
+from argparse import ArgumentParser
+from subprocess import Popen, PIPE
+
+
+def parse_args():
+    p = ArgumentParser()
+    p.add_argument('-b', '--bin-mapping', type=str,
+                   default='binary_mapping.txt',
+                   help='binary-source code mapping')
+    p.add_argument('-r', '--results', type=str, default='')
+    return p.parse_args()
 
 
 def read_results(stream):
@@ -19,20 +30,56 @@ def read_results(stream):
 
 def print_markdown_table(results):
     # header
-    print('| Rank | Experiment | CPU seconds | User time | Maximum memory |')
-    print('| :---: | :---: | :---: | :---: | :---: |')
+    print('| Rank | Experiment | CPU seconds | User time | '
+          'Maximum memory | Contributor |')
+    print('| :---: | :---: | :---: | :---: | :---: | :---: |')
     for rank, (src, res) in enumerate(sorted(
             results.iteritems(), key=lambda x: x[1][0])):
-        print('| {0} | {1} | {2} | {3} | {4} |'.format(
-            rank+1, src, res[0], res[1], res[2]))
+        print('| {0} | {1} | {2} | {3} | {4} | {5} | '.format(
+            rank+1, src, res[0], res[1], res[2],
+            ', '.join(res[3])))
+
+
+def read_binary_mapping(fn):
+    with open(fn) as f:
+        binaries = {}
+        for l in f:
+            fd = l.decode('utf8').strip().split('\t')
+            binaries[fd[0]] = fd[1]
+        return binaries
+
+
+def match_contributors(results, bin_map):
+    for binary, result in results.iteritems():
+        src = bin_map[binary]
+        ctr = get_contributors(src)
+        result.append(ctr)
+
+
+def get_contributors(fn):
+    p = Popen('git blame --incremental {}'.format(fn),
+              shell=True, stdout=PIPE).stdout.read()
+    contributors = []
+    for l in p.split('\n'):
+        if not l.startswith('author '):
+            continue
+        c = l.decode('utf8').split(' ')
+        nam = ' '.join(c[1:])
+        if nam not in contributors:
+            contributors.append(nam)
+    return contributors
 
 
 def main():
-    if len(argv) > 1:
+    args = parse_args()
+    results_fn = args.results
+    if results_fn:
         with open(argv[1]) as result_fn:
             results = read_results(result_fn)
     else:
         results = read_results(stdin)
+    binary_mapping = read_binary_mapping(args.bin_mapping)
+    match_contributors(results, binary_mapping)
     print_markdown_table(results)
 
 if __name__ == '__main__':
