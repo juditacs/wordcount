dae556d2bc6e62e169562078f6531f460c91ed82
belaustegui <cristian.alvb@gmail.com>
2016-03-26 11:54:12 +0100

    Use double headed annonymous functions
    We are using pattern-matching for the annonymous functions, which
    makes the code more readable and yields a small performance improvement.

--- elixir/wordcount.ex
+++ elixir/wordcount.ex
@@ -4,25 +4,17 @@ IO.stream(:stdio, :line)
 |> Enum.reduce(%{}, fn (line, acc) ->
   line
   |> String.split(~r/\s/, strip: true)
-  |> Enum.reduce(acc, fn (word, counter) ->
-    if String.length(word) == 0 do
-      counter
-    else
-      case Map.get(counter, word) do
-        nil -> Map.put(counter, word, 1)
-        prev_count -> Map.put(counter, word, prev_count + 1)
-      end
-    end
+  |> Enum.reduce(acc, fn
+      "", counter -> counter # Continue if the word is empty
+      word, counter -> Map.update(counter, word, 1, &(&1 + 1))
   end)
 end)
 # Transform the dictionary into a list of tuples with the form {word, appearances}
 |> Map.to_list
-# Sort words (if appearance count are equal, sort alphabetically)
-|> Enum.sort(fn ({word1, count1}, {word2, count2}) ->
-  cond do
-    count2 == count1 -> word1 < word2
-    true -> count1 > count2
-  end
+# Sort words
+|> Enum.sort(fn
+  {word1, count1}, {word2, count1} -> word1 < word2 # If appearance count is equal, sort alphabetically
+  {_, count1}, {_, count2} -> count1 > count2 # Sort by appearance count
 end)
 # Print the results
 |> Enum.each(fn ({word, count}) -> IO.puts("#{word}\t#{count}") end)
