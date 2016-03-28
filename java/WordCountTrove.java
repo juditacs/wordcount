c97a3066b339420cac50eecbc93c2dbba8ff6c51
Sam Van Oort <samvanoort@gmail.com>
2016-03-28 14:44:59 -0400

    Cleanup of optimized binary-based Java version

--- java/WordCountTrove.java
+++ java/WordCountTrove.java
@@ -13,9 +13,19 @@ import gnu.trove.map.hash.TObjectIntHashMap;
 import gnu.trove.strategy.HashingStrategy;
 import gnu.trove.procedure.TObjectIntProcedure;
 
-/** Word count for Java.
- *  Fast and memory-efficient because we use a primitives collection 
- *  and store Strings as UTF-8 bytes.
+/** Highly optimized word count algorithm for java incorporating multiple optimizations
+ *  Key tricks: 
+ *    - Work directly with raw bytes (safe for UTF-8), to reduce memory and avoid text decoding cost
+ *    - Buffer I/O, either using Buffered I/O classes or by explicitly reading chunks to an array
+ *    - Use Trove primitive collections (performance and memory savings, plus direct-iteration procedures)
+ *    - Process recurring tokens separately from singletons (big performance gain)
+ *    - Use a radix byte sort to sort large lists of bytestrings
+ *    - Sort strings using byte order (fudging the sort order slightly, for great performance savings)
+ *  
+ *  Authors:  
+ *   - Sam Van Oort <samvanoort@gmail.com>
+ *   - xupwup   
+ *   - Rick Hendricksen
  */
 class WordCountTrove {
 
@@ -62,53 +72,15 @@ class WordCountTrove {
         }
     }
 
-    // Constants for parsing
-    static final byte BYTE_SPACE = (byte)' ';
-    static final byte BYTE_TAB = (byte)'\t';
-    static final byte BYTE_NEWLINE = (byte)'\n';
-
-    /** Reads a chunk of characters, tokenizes it, and submits tokens
-    *  After execution, it shuffles residual characters to the start of the array
-    *  And returns the number of characters remaining to include in the next token
-    */
-    public static int tokenizeAndSubmitBlock(TObjectIntCustomHashMap<byte[]> counts,
-        final byte[] inputBuffer, int startIndex, int charsRead) {
-
-        int index = 0; // Start of current token
-        int endIndex = startIndex + charsRead;
-
-        // Go from start to end of current read
-        for (int i=startIndex; i<endIndex; i++) {
-            byte b = inputBuffer[i];
-
-            if (b == BYTE_SPACE || b == BYTE_NEWLINE || b == BYTE_TAB) { // New token
-                if (i != index) {
-                    byte[] word = new byte[i-index];
-                    System.arraycopy(inputBuffer, index, word, 0, word.length);                    
-                    submitWord(counts, word);
-                    index = i;
-                }
-                index++;
-            }
-        }
-
-        // Copy residual token content to beginning of the array, start going again
-        int residualSize =  endIndex - index;
-        if (residualSize > 0) {
-            System.arraycopy(inputBuffer, index, inputBuffer, 0, residualSize);
-            return residualSize;
-        }
-        return 0;
-    }
-
+    // Compares pairs of byte arrays by unsigned byte values, then by array length
     private static class ByteArrayComparator implements Comparator<byte[]> {
         @Override
         public int compare(byte[] b1, byte[] b2) {
-            // Sorting by byte value is less correct
+            // Sorting by unsigned byte value is less correct
             // but avoids very expensive UTF-8 decoding
             int limit = Math.min(b1.length, b2.length);
             for(int i=0; i<limit; i++) {
-                int diff = b1[i] - b2[i];
+                int diff = (b1[i] & 0xFF) - (b2[i] & 0xFF);
                 if (diff != 0) {
                     return diff;
                 }
@@ -121,7 +93,15 @@ class WordCountTrove {
             return o == this;
         }
     }
-	
+
+    private static final ByteArrayComparator BYTE_COMPARATOR_INSTANCE = new ByteArrayComparator();
+    
+    /** Sorts a list of byte arrays, switching between builtin sort & radix sort on bytes, depending on size of list
+     *  Arrays are sorted by the unsigned byte value of each element, and if identical then longest wins
+     *  Properties: recursive, not in-place, not null-safe, generates a bit of garbage
+     *  @param byteArrayList List of byte arrays to sort
+     *  @param byteOffset Offset of first byte in arryays to use in sorting, use 0 for the initial sorting
+     */
     static void fastRadixSort(ArrayList<byte[]> byteArrayList, int byteOffset) {
         if (byteArrayList.size() > 4096) { // Overheads not justified for small arrays
             ArrayList<byte[]>[] buckets = new ArrayList[256];
@@ -155,9 +135,47 @@ class WordCountTrove {
         } else {
             Collections.sort(byteArrayList, BYTE_COMPARATOR_INSTANCE);
          }
-    }
+    }    
 
-    private static final ByteArrayComparator BYTE_COMPARATOR_INSTANCE = new ByteArrayComparator();
+    // Token separators
+    static final byte BYTE_SPACE = (byte)' ';
+    static final byte BYTE_TAB = (byte)'\t';
+    static final byte BYTE_NEWLINE = (byte)'\n';
+
+    /** Tokenizes UTF-8 input and submits tokens, operating on raw bytes
+    *   This is safe because all separators are 1-byte encodings...
+    *   and all successive bytes of multibyte encodings have the high bit set
+    *   @return Length of residual at beginning of array, that is part of next token
+    */
+    public static int tokenizeAndSubmitBlock(TObjectIntCustomHashMap<byte[]> counts,
+        final byte[] inputBuffer, int startIndex, int length) {
+
+        int tokenStartIndex = 0;
+        int endIndex = startIndex + length;
+
+        // Go from start to end of current read
+        for (int i=startIndex; i<endIndex; i++) {
+            byte b = inputBuffer[i];
+
+            if (b == BYTE_SPACE || b == BYTE_NEWLINE || b == BYTE_TAB) { // Token end
+                if (i != tokenStartIndex) {
+                    byte[] word = new byte[i-tokenStartIndex];
+                    System.arraycopy(inputBuffer, tokenStartIndex, word, 0, word.length);                    
+                    submitWord(counts, word);
+                    tokenStartIndex = i;
+                }
+                tokenStartIndex++;
+            }
+        }
+
+        // Copy residual token content to beginning of the array, so we can read in more data
+        int residualSize =  endIndex - tokenStartIndex;
+        if (residualSize > 0) {
+            System.arraycopy(inputBuffer, tokenStartIndex, inputBuffer, 0, residualSize);
+            return residualSize;
+        }
+        return 0;
+    }
     
     private static void submitWord(TObjectIntCustomHashMap<byte[]> m, byte[] word) {
         m.adjustOrPutValue(word, 1, 1);        
@@ -171,7 +189,7 @@ class WordCountTrove {
         
         byte[] buff = new byte[8192];  // Limits maximum token size
 
-        // Read through chunks, tokenizing them directly on byte array
+        // Read through chunks, carrying over content that is part of a token
         int readAmount;
         int offset = 0;
         while ((readAmount = stdin.read(buff, offset, buff.length-offset)) > 0 ) {
@@ -188,9 +206,9 @@ class WordCountTrove {
         System.err.println("Creating Count objects for sorting");
         startTime = System.currentTimeMillis();
 
+        // Separating singleton tokens from multiples lets us store & sort them more efficiently
         final ArrayList<CountForWord> multiples = new ArrayList<CountForWord>(m.size()/2);
         final ArrayList<byte[]> singles = new ArrayList<byte[]>(m.size()/2);
-
         TObjectIntProcedure<byte[]> proc = new TObjectIntProcedure<byte[]>(){
             @Override
             public boolean execute(byte[] a, int b) {
@@ -220,12 +238,13 @@ class WordCountTrove {
 
         System.err.println("output...");
         startTime = System.currentTimeMillis();
-
+        // Output is buffered to reduce stream I/O overhead
         BufferedOutputStream out = new BufferedOutputStream(System.out);
         for (CountForWord c : multiples) {
             out.write(c.word);
             out.write(("\t"+c.count+'\n').getBytes(StandardCharsets.UTF_8));
         }
+        // Finally we write singletons out
         byte[] simpleSuffix = "\t1\n".getBytes(StandardCharsets.UTF_8);
         for (byte[] b : singles) {
             out.write(b);
