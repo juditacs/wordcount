d324a7545a0d2cc0d76d6edfe03769955d7b28ba
Norbert Melzer <timmelzer@gmail.com>
2016-03-28 12:46:43 +0200

    Added hackyish way of an ETS backed counter
    This improved runtime for the small testset (scripts/create_input.sh) by
    50 secs (~120 to ~70) at the cost of some memory, which went from
    ~1.65GiB to ~1.87GiB.

--- elixir/lib/wordcount.ex
+++ elixir/lib/wordcount.ex
@@ -1,13 +1,34 @@
 defmodule Wordcount do
+  defmodule Store do
+    def new(), do: :ets.new :wc, [:set, :named_table]
+
+    def count(tbl, word) do
+      case :ets.lookup(:wc, word) do
+        [{^word, cnt}] -> :ets.insert(:wc, {word, cnt + 1})
+        []             -> :ets.insert(:wc, {word, 1})
+      end
+      tbl
+    end
+
+    def to_list(tbl) do
+      :ets.foldl(fn {w, _} = entry, acc ->
+        new_acc = [entry|acc]
+        :ets.delete(tbl, w)
+        new_acc
+      end, [], tbl)
+    end
+  end
+
   def main([]), do: wordcount()
 
   defp wordcount() do
     IO.stream(:stdio, :line)
     |> Stream.flat_map(&Regex.scan(~r/\S+/, &1))
-    |> Enum.reduce(%{}, fn
-      "",   cnt -> cnt
-      word, cnt -> Map.update(cnt, word, 1, &(&1 + 1))
+    |> Enum.reduce(Store.new, fn
+      word, tbl ->
+        Store.count(tbl, word)
     end)
+    |> Store.to_list
     |> Enum.sort(fn
       {word1, count},  {word2, count}  -> word1 < word2
       {_,     count1}, {_,     count2} -> count1 > count2
@@ -16,3 +37,7 @@ defmodule Wordcount do
     |> (fn a -> IO.write(:stdio, a) end).()
   end
 end
+
+# Improvements:
+# 1. Try to implement Hash-Based-Map/use HashDict
+# 2. Try an ETS, propably using some Hash-MStuff
