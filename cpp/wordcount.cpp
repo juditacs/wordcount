61e9f1df08d0918a905e8fecce4262bb5b118436
Dmitry Andreev <dandreev@virtuozzo.com>
2016-03-29 11:20:26 +0300

    Use vector of tuples instead of vector of pairs

--- cpp/wordcount.cpp
+++ cpp/wordcount.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <unordered_map>
 #include <algorithm>
 
 using namespace std;
-
-class F {
-public:
-    using value_type = pair<int, string>;
-
-    bool operator()(value_type const& lhs, value_type const& rhs) {
-        if (lhs.first > rhs.first) return true;
-        if (lhs.first == rhs.first && lhs.second < rhs.second) return true;
-        return false;
-    }
-};
 
 int main() {
     unordered_map<string, int> m;
@@ -22,15 +12,15 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     while (cin >> s) {
-        ++m[s];
+        --m[s];
     }
-    vector<pair<int, string>> mvec;
+    vector<tuple<int, string>> mvec;
     for (auto& p: m) {
         mvec.emplace_back(p.second, move(p.first));
     }
-    sort(mvec.begin(), mvec.end(), F());
+    sort(mvec.begin(), mvec.end());
     for (const auto& p: mvec) {
-        cout << p.second << "\t" << p.first << "\n";
+        cout << get<1>(p) << "\t" << -get<0>(p) << "\n";
     }
 
 }
