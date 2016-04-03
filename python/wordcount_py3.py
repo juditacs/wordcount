4c5adcf642da757f99de1ddd56f78d1bbc8a7556
Sam Van Oort <samvanoort@gmail.com>
2016-04-03 13:03:29 -0400

    Python 3: uglyish way to sort words by count before sorting by words

--- python/wordcount_py3.py
+++ python/wordcount_py3.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 from collections import defaultdict
+from itertools import groupby
 from sys import stdin
 from sys import stdout
-import codecs
-
 
 def word_count():
     counter = defaultdict(int)
     for l in stdin.buffer:
         for word in l.split():
             counter[word] += 1
-    wordlist = [(word,count) for word,count in counter.items()]
-    del counter  # Free the memory, man!
-    wordlist.sort(key=lambda x: (-x[1], x[0]))
+    
+    # Ugly, but we're converting {word:count} to list(words) & (count, word_index)    
+    # This is because it is faster to sort the (count, word_index) objects
+    wordlist, countlist = list(), list()
+    i=0
+    for word, count in counter.items():
+        wordlist.append(word)
+        countlist.append((count, i))
+        i = i+1
 
-    # Print as bytes
-    for word, count in wordlist:
-        stdout.buffer.write(word + b'\t' + str(count).encode('utf-8')+b'\n')
+    del counter  # Free the memory, man!
+    countlist.sort(key=lambda x: -x[0])
+
+    # Group words by their counts, and sorting the lists of words with the same count
+    for count, word_indices in groupby(countlist, lambda x: x[0]):
+        suffix = b'\t' + str(count).encode('utf-8') + b'\n'
+        words = list(map(lambda y: wordlist[y[1]], word_indices))
+        words.sort()
+
+        for word in words:
+            stdout.buffer.write(word)
+            stdout.buffer.write(suffix)
 
 if __name__ == '__main__':
     word_count()
