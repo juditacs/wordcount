fb447f9193e97d1d60d925a5ddd8c3bc7eb48131
Sam Van Oort <samvanoort@gmail.com>
2016-04-03 13:15:42 -0400

    Python 3: group lists of word with the same count directly, don't try a fast integer sorting

--- python/wordcount_py3.py
+++ python/wordcount_py3.py
@@ -9,25 +9,19 @@ def word_count():
     for l in stdin.buffer:
         for word in l.split():
             counter[word] += 1
-    
-    # Ugly, but we're converting {word:count} to list(words) & (count, word_index)    
-    # This is because it is faster to sort the (count, word_index) objects
-    wordlist, countlist = list(), list()
-    i=0
+
+    groupedcounts = defaultdict(list)
     for word, count in counter.items():
-        wordlist.append(word)
-        countlist.append((count, i))
-        i = i+1
+        groupedcounts[count].append(word)
+    del counter
 
-    del counter  # Free the memory, man!
-    countlist.sort(key=lambda x: -x[0])
-
-    # Group words by their counts, and sorting the lists of words with the same count
-    for count, word_indices in groupby(countlist, lambda x: x[0]):
+    grouped_list = list(groupedcounts.items())
+    del groupedcounts
+    grouped_list.sort(key=lambda x: -x[0])
+    
+    for count, words in grouped_list:
         suffix = b'\t' + str(count).encode('utf-8') + b'\n'
-        words = list(map(lambda y: wordlist[y[1]], word_indices))
         words.sort()
-
         for word in words:
             stdout.buffer.write(word)
             stdout.buffer.write(suffix)
