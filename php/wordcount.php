b22601640a17352e5a72a95c49ff6e086981aa9a
Andrey Bukatov <andrey.bukatov@gmail.com>
2016-04-08 12:01:52 +0300

    Optimize performance and rename wordcount2.php to wordcount.php

--- php/wordcount.php
+++ php/wordcount.php
@@ -1,43 +1,37 @@
 #!/usr/bin/php
 <?php
+
 ini_set('memory_limit', '16384M');
+
 $stdin = fopen('php://stdin', 'r');
-//$stdin = fopen('test', 'r');
 
-
-$array = array();
+$delimiters = " \n\t";
+$countedWords = array();
 while (false !== ($line = fgets($stdin))) {
-  $words = preg_split('/\s+/', $line);
-    foreach($words as $word){
-        if(empty($word) &&  $word !== "0"){
-            continue;
+    $token = strtok($line, $delimiters);
+    while (false !== $token) {
+        if (isset($countedWords[$token])) {
+            ++$countedWords[$token];
+        } else {
+            $countedWords[$token] = 1;
         }
-        if (!array_key_exists($word,$array)){
-            $array[$word] = 1;
-            continue;
-        }
-        $array[$word]++;
+
+        $token = strtok($delimiters);
     }
-
-
 }
+
 fclose($stdin);
-$array2 = array();
 
-foreach($array as $key => $value){
-
-    if (!array_key_exists($value,$array2)){
-        $array2[$value] = array($key);
-        continue;
-    }
-    $array2[$value][] =(string) $key;
+$groupedByCountWords = array();
+foreach ($countedWords as $word => $count) {
+    $groupedByCountWords[$count][] = $word;
 }
 
+krsort($groupedByCountWords);
 
-krsort($array2);
-foreach($array2 as $count => $wordsArray){
-    sort ($wordsArray);
-    foreach($wordsArray as $word) {
-        print "$word\t$count\n";
+foreach ($groupedByCountWords as $count => $words) {
+    sort($words, SORT_STRING);
+    foreach ($words as $word) {
+        printf("%s\t%s\n", $word, $count);
     }
 }
